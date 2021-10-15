090ab485911d7df6762a83f5aec085557c06bec3
Daniel Lozada <67969956+dflr10@users.noreply.github.com>
2021-10-15 01:05:22 -0500

    deployment changes

--- pethomeProject/settings.py
+++ pethomeProject/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-#import django_heroku
+import django_heroku
 from pathlib import Path
 from datetime import timedelta
 import os
@@ -160,4 +160,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
